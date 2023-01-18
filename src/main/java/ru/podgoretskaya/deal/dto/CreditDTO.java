e8393433473920976af1c8eacd87d85958be9d08
Olya-Breskina <breskina-olya@mail.ru>
2023-01-18 21:09:26 +0300

    entity с enum  и json созданы

---
+++ src/main/java/ru/podgoretskaya/deal/dto/CreditDTO.java
@@ -0,0 +1,22 @@
+package ru.podgoretskaya.deal.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class CreditDTO {
+    private BigDecimal amount;
+    private Integer term;
+    private BigDecimal monthlyPayment;
+    private BigDecimal rate;
+    private BigDecimal psk;
+    private Boolean isInsuranceEnabled;
+    private Boolean isSalaryClient;
+    private List<PaymentScheduleElement> paymentSchedule;
+}
